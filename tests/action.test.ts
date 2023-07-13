2c5dc4a17a1db34b788a65ea81a858f8c38dc618
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-07-13 19:11:25 -0300

    test: improve tests

--- tests/action.test.ts
+++ tests/action.test.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core'
 import {describe, expect, jest, test} from '@jest/globals'
 
 import Action from '../src/action'
+import {GitHubService} from '../src/github.service'
 import {
   RenderDeployStatus,
   RenderErrorResponse,
@@ -10,8 +11,6 @@ import {
 import {getAxiosError} from './helpers/axios.helper'
 
 describe('inputs', () => {
-  afterEach(() => jest.clearAllMocks())
-
   test('should throw an error if the input "service_id" is missing', async () => {
     const coreSpy = jest.spyOn(core, 'setFailed')
 
@@ -44,20 +43,20 @@ describe('inputs', () => {
     process.env['INPUT_GITHUB_DEPLOYMENT'] = 'false'
     process.env['GITHUB_REPOSITORY'] = 'action/test'
 
-    const renderSpy = jest.spyOn(RenderService.prototype, 'triggerDeploy')
+    const spy = jest
+      .spyOn(RenderService.prototype, 'triggerDeploy')
+      .mockResolvedValueOnce('')
 
     await new Action().run()
 
-    expect(renderSpy).toHaveBeenCalledTimes(1)
-    expect(renderSpy).toHaveBeenCalledWith({
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith({
       clearCache: true
     })
   })
 })
 
 describe('deploy', () => {
-  afterEach(() => jest.clearAllMocks())
-
   test('should trigger a deploy', async () => {
     process.env['INPUT_SERVICE_ID'] = 'my service id'
     process.env['INPUT_API_KEY'] = 'my api key'
@@ -68,7 +67,7 @@ describe('deploy', () => {
 
     const spy = jest
       .spyOn(RenderService.prototype, 'triggerDeploy')
-      .mockResolvedValue('123')
+      .mockResolvedValueOnce('id')
 
     await new Action().run()
 
@@ -83,6 +82,9 @@ describe('deploy', () => {
     process.env['INPUT_GITHUB_DEPLOYMENT'] = 'false'
     process.env['GITHUB_REPOSITORY'] = 'action/test'
 
+    jest
+      .spyOn(RenderService.prototype, 'triggerDeploy')
+      .mockResolvedValueOnce('id')
     const spy = jest
       .spyOn(RenderService.prototype, 'verifyDeployStatus')
       .mockResolvedValue(RenderDeployStatus.LIVE)
@@ -93,8 +95,6 @@ describe('deploy', () => {
   })
 
   describe('deploy error handling', () => {
-    afterEach(() => jest.clearAllMocks())
-
     test('should exit if the deploy status is "build_failed"', async () => {
       process.env['INPUT_SERVICE_ID'] = 'my service id'
       process.env['INPUT_API_KEY'] = 'my api key'
@@ -103,6 +103,9 @@ describe('deploy', () => {
       process.env['INPUT_GITHUB_DEPLOYMENT'] = 'false'
       process.env['GITHUB_REPOSITORY'] = 'action/test'
 
+      jest
+        .spyOn(RenderService.prototype, 'triggerDeploy')
+        .mockResolvedValueOnce('id')
       const spy = jest
         .spyOn(RenderService.prototype, 'verifyDeployStatus')
         .mockResolvedValue(RenderDeployStatus.BUILD_FAILED)
@@ -124,6 +127,9 @@ describe('deploy', () => {
       process.env['INPUT_GITHUB_DEPLOYMENT'] = 'false'
       process.env['GITHUB_REPOSITORY'] = 'action/test'
 
+      jest
+        .spyOn(RenderService.prototype, 'triggerDeploy')
+        .mockResolvedValueOnce('id')
       const spy = jest
         .spyOn(RenderService.prototype, 'verifyDeployStatus')
         .mockResolvedValue(RenderDeployStatus.CANCELED)
@@ -145,6 +151,9 @@ describe('deploy', () => {
       process.env['INPUT_GITHUB_DEPLOYMENT'] = 'false'
       process.env['GITHUB_REPOSITORY'] = 'action/test'
 
+      jest
+        .spyOn(RenderService.prototype, 'triggerDeploy')
+        .mockResolvedValueOnce('id')
       const spy = jest
         .spyOn(RenderService.prototype, 'verifyDeployStatus')
         .mockResolvedValue(RenderDeployStatus.DEACTIVATED)
@@ -166,6 +175,9 @@ describe('deploy', () => {
       process.env['INPUT_GITHUB_DEPLOYMENT'] = 'false'
       process.env['GITHUB_REPOSITORY'] = 'action/test'
 
+      jest
+        .spyOn(RenderService.prototype, 'triggerDeploy')
+        .mockResolvedValueOnce('id')
       const spy = jest
         .spyOn(RenderService.prototype, 'verifyDeployStatus')
         .mockResolvedValue(RenderDeployStatus.UPLOAD_FAILED)
@@ -181,9 +193,7 @@ describe('deploy', () => {
   })
 })
 
-describe('error handling', () => {
-  afterEach(() => jest.clearAllMocks())
-
+describe('Render error handling', () => {
   test('should exit if the server retuns 401', async () => {
     process.env['INPUT_SERVICE_ID'] = 'my service id'
     process.env['INPUT_API_KEY'] = 'my api key'
@@ -196,9 +206,7 @@ describe('error handling', () => {
 
     jest
       .spyOn(RenderService.prototype, 'triggerDeploy')
-      .mockImplementationOnce(() => {
-        throw getAxiosError(401)
-      })
+      .mockRejectedValueOnce(getAxiosError(401))
 
     await new Action().run()
 
@@ -218,9 +226,7 @@ describe('error handling', () => {
 
     jest
       .spyOn(RenderService.prototype, 'triggerDeploy')
-      .mockImplementationOnce(() => {
-        throw getAxiosError(404)
-      })
+      .mockRejectedValueOnce(getAxiosError(404))
 
     await new Action().run()
 
@@ -240,9 +246,7 @@ describe('error handling', () => {
 
     jest
       .spyOn(RenderService.prototype, 'triggerDeploy')
-      .mockImplementationOnce(() => {
-        throw getAxiosError(429)
-      })
+      .mockRejectedValueOnce(getAxiosError(429))
 
     await new Action().run()
 
@@ -262,9 +266,7 @@ describe('error handling', () => {
 
     jest
       .spyOn(RenderService.prototype, 'triggerDeploy')
-      .mockImplementationOnce(() => {
-        throw getAxiosError(500)
-      })
+      .mockRejectedValueOnce(getAxiosError(500))
 
     await new Action().run()
 
@@ -284,9 +286,7 @@ describe('error handling', () => {
 
     jest
       .spyOn(RenderService.prototype, 'triggerDeploy')
-      .mockImplementationOnce(() => {
-        throw getAxiosError(503)
-      })
+      .mockRejectedValueOnce(getAxiosError(503))
 
     await new Action().run()
 
@@ -306,13 +306,33 @@ describe('error handling', () => {
 
     jest
       .spyOn(RenderService.prototype, 'triggerDeploy')
-      .mockImplementationOnce(() => {
-        throw getAxiosError(507)
-      })
+      .mockRejectedValueOnce(getAxiosError(507))
 
     await new Action().run()
 
     expect(coreSpy).toHaveBeenCalledTimes(1)
     expect(coreSpy).toHaveBeenCalledWith('Unexpected error')
+  })
+})
+
+describe('GitHub deployment', () => {
+  afterEach(() => jest.clearAllMocks())
+
+  test('should be optional', async () => {
+    process.env['INPUT_SERVICE_ID'] = 'my service id'
+    process.env['INPUT_API_KEY'] = 'my api key'
+    process.env['INPUT_CLEAR_CACHE'] = 'false'
+    process.env['INPUT_WAIT_DEPLOY'] = 'false'
+    process.env['INPUT_GITHUB_DEPLOYMENT'] = 'false'
+    process.env['GITHUB_REPOSITORY'] = 'action/test'
+
+    jest
+      .spyOn(RenderService.prototype, 'triggerDeploy')
+      .mockResolvedValueOnce('id')
+    const spy = jest.spyOn(GitHubService.prototype, 'createDeployment')
+
+    await new Action().run()
+
+    expect(spy).toHaveBeenCalledTimes(1)
   })
 })
