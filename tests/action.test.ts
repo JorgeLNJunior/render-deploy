6dc8935eabaaed09e053e06bf6d623ddf39450db
JorgeLNJunior <jorgelnjunior@gmail.com>
2026-01-17 16:31:53 -0300

    test: add tests for branch option feature

--- tests/action.test.ts
+++ tests/action.test.ts
@@ -96,6 +96,76 @@ describe('Inputs', () => {
       commitSHA,
     })
   })
+
+  test('should deploy the latest commit of a branch if specified', async () => {
+    const serviceID = 'my service id'
+    const commitSHA = '7723293c4a49e7b3aa60ba196baf9a5a0bc5fc02'
+    const branch = 'refactor-database-access'
+    const clearCache = false
+
+    process.env['GITHUB_REPOSITORY'] = 'action/test'
+    process.env['GITHUB_REF'] = 'main'
+    process.env['INPUT_SERVICE_ID'] = serviceID
+    process.env['INPUT_API_KEY'] = 'my api key'
+    process.env['INPUT_CLEAR_CACHE'] = String(clearCache)
+    process.env['INPUT_BRANCH'] = branch
+    process.env['INPUT_WAIT_DEPLOY'] = 'true'
+    process.env['INPUT_GITHUB_DEPLOYMENT'] = 'false'
+
+    vi.spyOn(RenderService.prototype, 'triggerDeploy').mockResolvedValueOnce(
+      'id',
+    )
+    vi.spyOn(
+      GitHubService.prototype,
+      'getBranchLatestCommit',
+    ).mockResolvedValueOnce(commitSHA)
+    const spy = vi
+      .spyOn(RenderService.prototype, 'triggerDeploy')
+      .mockResolvedValueOnce(RenderDeployStatus.LIVE)
+
+    await new Action().run()
+
+    expect(spy).toHaveBeenCalledWith({
+      clearCache,
+      commitSHA,
+    })
+  })
+
+  test('should prefer "commit_sha" instead of "branch" when both are specified', async () => {
+    const serviceID = 'my service id'
+    const commitSHA = '7723293c4a49e7b3aa60ba196baf9a5a0bc5fc02'
+    const branch = 'refactor-database-access'
+    const clearCache = false
+
+    process.env['GITHUB_REPOSITORY'] = 'action/test'
+    process.env['GITHUB_REF'] = 'main'
+    process.env['INPUT_SERVICE_ID'] = serviceID
+    process.env['INPUT_API_KEY'] = 'my api key'
+    process.env['INPUT_CLEAR_CACHE'] = String(clearCache)
+    process.env['INPUT_COMMIT_SHA'] = commitSHA
+    process.env['INPUT_BRANCH'] = branch
+    process.env['INPUT_WAIT_DEPLOY'] = 'true'
+    process.env['INPUT_GITHUB_DEPLOYMENT'] = 'false'
+
+    vi.spyOn(RenderService.prototype, 'triggerDeploy').mockResolvedValueOnce(
+      'id',
+    )
+    const branchCommitSpy = vi.spyOn(
+      GitHubService.prototype,
+      'getBranchLatestCommit',
+    )
+    const deploySpy = vi
+      .spyOn(RenderService.prototype, 'triggerDeploy')
+      .mockResolvedValueOnce(RenderDeployStatus.LIVE)
+
+    await new Action().run()
+
+    expect(branchCommitSpy).not.toHaveBeenCalled()
+    expect(deploySpy).toHaveBeenCalledWith({
+      clearCache,
+      commitSHA,
+    })
+  })
 })
 
 describe('Deploy', () => {
